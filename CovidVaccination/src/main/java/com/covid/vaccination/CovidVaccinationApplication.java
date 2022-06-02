15fbb9ada179227eddb2b48b401dd7787a740dda
abusalem <abusalemmangalwedhe@gmail.com>
2022-06-02 21:23:09 +0530

    Project Creation Done!

---
+++ CovidVaccination/src/main/java/com/covid/vaccination/CovidVaccinationApplication.java
@@ -0,0 +1,13 @@
+package com.covid.vaccination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidVaccinationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidVaccinationApplication.class, args);
+	}
+
+}
---
+++ CovidVaccination/src/test/java/com/covid/vaccination/CovidVaccinationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.covid.vaccination;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidVaccinationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
