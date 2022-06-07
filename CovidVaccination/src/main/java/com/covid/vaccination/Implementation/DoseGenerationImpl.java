7568705a535c87f1f72c8565f2b48f5fcc669b06
abusalem <abusalemmangalwedhe@gmail.com>
2022-06-07 13:50:03 +0530

    Error Resolved!

--- CovidVaccination/src/main/java/com/covid/vaccination/Implementation/DoseGenerationImpl.java
+++ CovidVaccination/src/main/java/com/covid/vaccination/Implementation/DoseGenerationImpl.java
@@ -10,8 +10,12 @@ import com.covid.vaccination.Repository.Dose2Repository;
 import com.covid.vaccination.Repository.DoseGenerationRepository;
 import com.covid.vaccination.Repository.DoseRepository;
 import com.covid.vaccination.Service.DoseGenerationService;
+import org.hibernate.exception.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class DoseGenerationImpl implements DoseGenerationService {
@@ -24,24 +28,31 @@ public class DoseGenerationImpl implements DoseGenerationService {
 
     @Autowired
     public DoseRepository doseRepository;
+    @Autowired
+    public Dose1Impl doseimpl;
 
     public String generatedDose(DoctorDoseGeneration doctorDoseGeneration) {
 
         Dose1 dose1 = dose1Repository.getDose1ByUser_id(doctorDoseGeneration.getUser_id());
         Dose2 dose2 = dose2Repository.getDose2ByUser_id(doctorDoseGeneration.getUser_id());
         Dose dose = doseRepository.getDoseByUser_id(doctorDoseGeneration.getUser_id());
-        Dose d= new Dose();
 
         if (dose1 == null && dose2 == null) {
             doseGenerationRepository.save(doctorDoseGeneration);
             return "Dose 1 SuccessFully Vaccinated";
         }
+
         else if ((dose1 != null) && dose2 == null) {
-            doseGenerationRepository.save(doctorDoseGeneration);
-            return "Dose 2 Successfully Vaccinated";
-        } else if (dose1.getUser_id() == doctorDoseGeneration.getUser_id() && dose2.getUser_id() == doctorDoseGeneration.getUser_id()) {
+            if(doctorDoseGeneration.getDose1()==null){
+                doseGenerationRepository.save(doctorDoseGeneration);
+                return "Dose 2 Successfully Vaccinated";
+            }else {
+                return "Already Dose 1 Completed";
+            }
+
+        } else {
             return "You Are Fully Vaccinated";
         }
-        return "Failed";
+
     }
 }
